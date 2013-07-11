d17feeeb42eedd86855016944a70cea25fa95bc7
xuheng <pk3338455>
2013-07-11 19:37:45 +0800

    [fixed]byxuheng

--- dialogs/image/image.js
+++ dialogs/image/image.js
@@ -1,6 +1,15 @@
-(function(){
+(function () {
     UE.registerWidget('image', {
-        tpl: "",
+        tpl: "<div id=\"edui-image-Jwrapper\">" +
+            "<ul class=\"edui-nav\">" +
+            "<li><a href=\"#edui-image-Jlocal\">本地上传</a></li>" +
+            "<li><a href=\"#edui-image-JimgSearch\">图片搜索</a></li>" +
+            "</ul>" +
+            "<div class=\"edui-tab-content\">" +
+            "<div id=\"edui-image-Jlocal\" class=\"edui-tab-pane active\"></div>" +
+            "<div id=\"edui-image-JimgSearch\" class=\"edui-tab-pane\"></div>" +
+            "</div>" +
+            "</div>",
         initContent: function (editor) {
             var lang = editor.getLang('image');
             if (lang) {
@@ -9,16 +18,16 @@
             this.root().html(html);
         },
         initEvent: function (editor, $w) {
-
+            $.eduitab({selector:"#edui-image-Jwrapper",context:document});
         },
         buttons: {
             'ok': {
                 label: '插入图片',
-                exec: function(){
+                exec: function () {
 
                 }
             },
-            'cancel':{}
+            'cancel': {}
         }
 
     })
