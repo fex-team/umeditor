720001ab091062a9f2f12227aaa6116130ab977c
hancong03 <121371790@qq.com>
2013-07-11 14:51:32 +0800

    微调

--- _src/adapter/emotion.js
+++ _src/adapter/emotion.js
@@ -3,15 +3,13 @@
  */
 
 UE.registerUI( 'emotion', function( name ){
-    var editor = this,
-        emotionUrl = editor.options.UEDITOR_HOME_URL + '/dialogs/emotion/emotion.html';
 
-    emotionPopup = $.eduiemotion({
-        url: emotionUrl
+    var me = this;
+
+    return $.eduiemotion({
+        url: me.options.UEDITOR_HOME_URL + '/dialogs/emotion/emotion.html'
     }).edui().on('beforeshow',function(){
         UE.setActiveWidget(this.root())
     }).root();
-
-    return emotionPopup;
 
 } );
