b3ec39c6cbac3016aae7820bdd988989f23908af
Jinqn <459452372@qq.com>
2013-08-19 13:25:49 +0800

    [add]添加拖拽上传的插件 by Jinqn

---
+++ _src/plugins/dropfile.js
@@ -0,0 +1,40 @@
+/*
+ * 拖放文件到编辑器上传
+ */
+UE.plugins['dropfile'] = function() {
+    var me = this;
+    me.addListener('ready', function(){
+        me.$body.on('drop',function (e) {
+            //获取文件列表
+            var fileList = e.originalEvent.dataTransfer.files;
+            var hasImg = false;
+            $.each(fileList, function (i, f) {
+                if (/^image/.test(f.type)) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("post", me.getOpt('imageUrl'), true);
+                    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+                    //模拟数据
+                    var fd = new FormData();
+                    fd.append(me.getOpt('imageFieldName'), f);
+                    fd.append('type', 'ajax');
+
+                    xhr.send(fd);
+                    xhr.addEventListener('load', function (e) {
+                        var picLink = me.getOpt('imagePath') + e.target.response;
+                        me.execCommand('insertimage', {
+                            src: picLink,
+                            _src: picLink
+                        });
+                    });
+                    hasImg = true;
+                }
+            });
+            if (hasImg) {
+                e.preventDefault();
+            }
+        }).on('dragover', function (e) {
+            e.preventDefault();
+        });
+    });
+};
