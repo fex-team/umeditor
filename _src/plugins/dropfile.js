8bf13baf165f2b9698116cf85fffb7fda24aecef
Jinqn <459452372@qq.com>
2013-08-20 15:18:03 +0800

    [fix]by Jinqn

--- _src/plugins/dropfile.js
+++ _src/plugins/dropfile.js
@@ -13,34 +13,38 @@ UE.plugins['dropfile'] = function() {
                     //获取文件列表
                     var fileList = e.originalEvent.dataTransfer.files;
                     var hasImg = false;
-                    $.each(fileList, function (i, f) {
-                        if (/^image/.test(f.type)) {
-                            var xhr = new XMLHttpRequest();
-                            xhr.open("post", me.getOpt('imageUrl'), true);
-                            xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                    if(fileList) {
+                        $.each(fileList, function (i, f) {
+                            if (/^image/.test(f.type)) {
+                                var xhr = new XMLHttpRequest();
+                                xhr.open("post", me.getOpt('imageUrl'), true);
+                                xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
-                            //模拟数据
-                            var fd = new FormData();
-                            fd.append(me.getOpt('imageFieldName') || 'upfile', f);
-                            fd.append('type', 'ajax');
+                                //模拟数据
+                                var fd = new FormData();
+                                fd.append(me.getOpt('imageFieldName') || 'upfile', f);
+                                fd.append('type', 'ajax');
 
-                            xhr.send(fd);
-                            xhr.addEventListener('load', function (e) {
-                                var picLink = me.getOpt('imagePath') + e.target.response;
-                                if(picLink) {
-                                    me.execCommand('insertimage', {
-                                        src: picLink,
-                                        _src: picLink
-                                    });
-                                }
-                            });
-                            hasImg = true;
-                        }
-                    });
+                                xhr.send(fd);
+                                xhr.addEventListener('load', function (e) {
+                                    var picLink = me.getOpt('imagePath') + e.target.response;
+                                    if(picLink) {
+                                        me.execCommand('insertimage', {
+                                            src: picLink,
+                                            _src: picLink
+                                        });
+                                    }
+                                });
+                                hasImg = true;
+                            }
+                        });
+                        if(hasImg) e.preventDefault();
+                    }
                 }
-                e.preventDefault();
             }).on('dragover', function (e) {
-                e.preventDefault();
+                if(e.originalEvent.dataTransfer.types[0] == 'Files') {
+                    e.preventDefault();
+                }
             });
         });
     }
