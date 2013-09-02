8df10b9ef01667b55b6554650c299e1c343643fb
hancong03 <121371790@qq.com>
2013-09-02 16:48:31 +0800

    用例补全

--- _test/adapter/combobox.js
+++ _test/adapter/combobox.js
@@ -11,12 +11,19 @@ test( '检测combobox的控制否正常', function() {
     stop();
     editor.ready(function(){
         setTimeout(function () {
-            var editor = te.obj[0];
-            $( ".edui-btn-name-fontfamily", editor.container).trigger("click");
-            setTimeout(function () {
-                equal( $( ".edui-combobox-fontfamily", editor.container ).css("display"), "block", '字体类型combobox打开正常' );
-                start();
-            }, 200);
+            var editor = te.obj[0],
+                components = [ "paragraph", "fontfamily", "fontsize" ];
+
+            for ( var i = 0, component; component = components[ i ]; i++ ) {
+
+                $( ".edui-btn-name-" + component, editor.container).trigger("click");
+
+                equal( $( ".edui-combobox-" + component , editor.container ).css("display"), "block", component+' combobox打开正常' );
+
+            }
+
+            start();
+
         }, 200);
     });
 
