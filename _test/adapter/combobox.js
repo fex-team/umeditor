b1d48b5783f44e15121689fef29ca0151c43a4f4
hancong03 <121371790@qq.com>
2013-09-02 17:16:13 +0800

    用例补全

--- _test/adapter/combobox.js
+++ _test/adapter/combobox.js
@@ -12,7 +12,8 @@ test( '检测combobox的控制否正常', function() {
     editor.ready(function(){
         setTimeout(function () {
             var editor = te.obj[0],
-                components = [ "paragraph", "fontfamily", "fontsize" ];
+                components = [ "paragraph", "fontfamily", "fontsize" ],
+                colors = [ "forecolor", "backcolor" ];
 
             for ( var i = 0, component; component = components[ i ]; i++ ) {
 
@@ -21,6 +22,16 @@ test( '检测combobox的控制否正常', function() {
                 equal( $( ".edui-combobox-" + component , editor.container ).css("display"), "block", component+' combobox打开正常' );
 
             }
+
+            for ( var i = 0, color; color = colors[ i ]; i++ ) {
+
+                $( ".edui-splitbutton-"+ color +" .edui-dropdown-toggle", editor.container).trigger("click");
+
+                equal( $( ".edui-colorpicker-" + color , editor.container).parents(".edui-popup").css("display"), "block", component+' combobox打开正常' );
+
+            }
+
+            $(document.body).trigger("click");
 
             start();
 
