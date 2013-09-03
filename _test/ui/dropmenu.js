bec1b0031dfddbbc4d77151f5f043928f338204b
Jinqn <459452372@qq.com>
2013-09-03 13:33:58 +0800

    [fix]by Jinqn

--- _test/ui/dropmenu.js
+++ _test/ui/dropmenu.js
@@ -5,7 +5,7 @@
  * Time: 下午1:28
  * To change this template use File | Settings | File Templates.
  */
-module('ui.dropmenu.js');
+module('ui.dropmenu');
 
 test('dropmenu--初始化', function () {
     var div = document.body.appendChild(document.createElement('div'));
@@ -17,11 +17,24 @@ test('dropmenu--初始化', function () {
             {"value":"lower-alpha","label":"a,b,c..."},
             {"value":"lower-roman","label":"i,ii,iii..."},
             {"value":"upper-alpha","label":"A,B,C..."},
-            {"value":"upper-roman","label":"I,II,III..."}
+            {"label":"I,II,III...",data:[
+                {"value":"decimal","label":"1,2,3..."},
+                {"value":"lower-alpha","label":"a,b,c..."},
+                {"value":"lower-roman","label":"i,ii,iii..."},
+                {"value":"upper-alpha","label":"A,B,C..."},
+                {"value":"upper-roman","label":"I,II,III..."}
+            ]}
         ],click:function(evt, val){
             equal(value, val, '检查菜单点击的value是否正确');
             div.parentNode.removeChild(div);
             start();
+
+            $dropMenuWidget.edui().val('upper-alpha');
+            equal($dropMenuWidget.edui().val(), 'upper-alpha', '检查设置菜单值是否正常');
+
+            $dropMenuWidget.edui().disabled(true);
+            $dropMenuWidget.find("li").hasClass('disabled');
+            equal($dropMenuWidget.find("li").hasClass('disabled'), true, '检查选项失效后，菜单项是否有disabled的class');
         }}).appendTo(div);
 
     var $btn = $.eduibutton({
@@ -30,21 +43,9 @@ test('dropmenu--初始化', function () {
     }).appendTo(div);
 
     $dropMenuWidget.edui().show($btn);
+    item = $dropMenuWidget.find("li")[0];
+    value = $(item).data('value');
 
-    setTimeout(function(){
-
-        var isshow = $dropMenuWidget.css("display") != "none";
-        equal(isshow, true, '检查菜单是否显示');
-
-        $dropMenuWidget.edui().hide();
-        var ishide = $dropMenuWidget.css("display") == "none";
-        equal(ishide, true, '检查菜单是否隐藏');
-
-        item = $dropMenuWidget.find("li")[0];
-        value = $(item).data('value');
-
-        ua.click(item);
-
-    },100);
+    ua.click(item);
     stop();
 });
--- _test/ui/popup.js
+++ _test/ui/popup.js
@@ -5,3 +5,47 @@
  * Time: 下午5:18
  * To change this template use File | Settings | File Templates.
  */
+/**
+ * Created by JetBrains PhpStorm.
+ * User: dongyancen
+ * Date: 12-4-12
+ * Time: 下午4:47
+ * To change this template use File | Settings | File Templates.
+ */
+module( 'ui.popup' );
+
+test('popup--初始化', function () {
+    var div = document.body.appendChild(document.createElement('div'));
+    $(div).attr('id', 'test');
+
+    var $btn = $.eduibutton({
+        icon: "bold",
+        title: "测试"
+    }).appendTo(div);
+
+    var $popupWidget = $.eduipopup({
+        subtpl: '<span class="test">popup text</span>',
+        width: '100',
+        height: '100'
+    });
+
+    $popupWidget.edui().show($btn,{
+        offsetTop:-5,
+            offsetLeft:10,
+            caretLeft:11,
+            caretTop:-8
+    });
+
+    setTimeout(function(){
+
+        var isshow = $popupWidget.css("display") != "none";
+        equal(isshow, true, '检查popup是否显示');
+
+        $popupWidget.edui().hide();
+        var ishide = $popupWidget.css("display") == "none";
+        equal(ishide, true, '检查popup是否隐藏');
+
+        start();
+    },100);
+    stop();
+});
