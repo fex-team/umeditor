75d70abc3c377b18928eefbe3f87a38e97bce8d1
campaign <zhanyi1022@gmail.com>
2013-09-11 18:25:04 +0800

    by zhanyi

--- _src/plugins/dropfile.js
+++ _src/plugins/dropfile.js
@@ -9,36 +9,39 @@ UM.plugins['dropfile'] = function() {
     if( me.getOpt('dropFileEnabled') && window.FormData && window.FileReader) {
         me.addListener('ready', function(){
             me.$body.on('drop',function (e) {
-                //获取文件列表
-                var fileList = e.originalEvent.dataTransfer.files;
-                var hasImg = false;
-                if(fileList) {
-                    $.each(fileList, function (i, f) {
-                        if (/^image/.test(f.type)) {
-                            var xhr = new XMLHttpRequest();
-                            xhr.open("post", me.getOpt('imageUrl'), true);
-                            xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                try{
+                    //获取文件列表
+                    var fileList = e.originalEvent.dataTransfer.files;
+                    var hasImg = false;
+                    if(fileList) {
+                        $.each(fileList, function (i, f) {
+                            if (/^image/.test(f.type)) {
+                                var xhr = new XMLHttpRequest();
+                                xhr.open("post", me.getOpt('imageUrl'), true);
+                                xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
-                            //模拟数据
-                            var fd = new FormData();
-                            fd.append(me.getOpt('imageFieldName') || 'upfile', f);
-                            fd.append('type', 'ajax');
+                                //模拟数据
+                                var fd = new FormData();
+                                fd.append(me.getOpt('imageFieldName') || 'upfile', f);
+                                fd.append('type', 'ajax');
 
-                            xhr.send(fd);
-                            xhr.addEventListener('load', function (e) {
-                                var picLink = me.getOpt('imagePath') + e.target.response;
-                                if(picLink) {
-                                    me.execCommand('insertimage', {
-                                        src: picLink,
-                                        _src: picLink
-                                    });
-                                }
-                            });
-                            hasImg = true;
-                        }
-                    });
-                    if(hasImg) e.preventDefault();
-                }
+                                xhr.send(fd);
+                                xhr.addEventListener('load', function (e) {
+                                    var picLink = me.getOpt('imagePath') + e.target.response;
+                                    if(picLink) {
+                                        me.execCommand('insertimage', {
+                                            src: picLink,
+                                            _src: picLink
+                                        });
+                                    }
+                                });
+                                hasImg = true;
+                            }
+                        });
+                        if(hasImg) e.preventDefault();
+                    }
+                }catch(e){}
+
             }).on('dragover', function (e) {
                     if(e.originalEvent.dataTransfer.types[0] == 'Files') {
                         e.preventDefault();
