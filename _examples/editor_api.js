1a9f8c003bb10da6786b9bc6d42d6931f87952a4
Jinqn <459452372@qq.com>
2014-01-13 15:30:29 +0800

    by Jinqn

--- _examples/editor_api.js
+++ _examples/editor_api.js
@@ -4,7 +4,6 @@
 (function (){
     var paths  = [
             'editor.js',
-            'jqueryapi.js',
             'core/browser.js',
             'core/utils.js',
             'core/EventBase.js',
