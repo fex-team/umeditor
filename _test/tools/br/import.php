b6fd0be6bccbc1024e65061f5f6604e327800c55
yancend <dongyancen@baidu.com>
2014-01-20 12:16:25 +0800

    [test]删除import.php里一些注释

--- _test/tools/br/import.php
+++ _test/tools/br/import.php
@@ -12,10 +12,7 @@ if(!Config::$DEBUG){
  * Copyright 2009 Baidu Inc. All rights reserved.
  *
  * path: import.php
- * author: berg
- * version: 1.0
- * date: 2010/07/18 23:57:52
- *
+
  * @fileoverview * import.js的php版本
  * 接受一个f参数，格式和import.js相同，自动合并js并输出
  * 此外，本脚本支持引入一个包所有文件（其实也就是一个目录下的所有js文件，**不递归**）
