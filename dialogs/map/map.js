5e8cefdbcf95cb123606dc4296a8cbd55738f4df
Jinqn <459452372@qq.com>
2014-04-21 23:17:28 +0800

    修改地图插件

--- dialogs/map/map.js
+++ dialogs/map/map.js
@@ -230,7 +230,7 @@
                         center = widget.map.getCenter(),
                         zoom = widget.map.getZoom(),
                         size = widget.map.getSize(),
-                        point = widget.marker.Q;
+                        point = widget.marker.getPosition();
 
                     if (widget.root().find(".edui-map-dynamic")[0].checked) {
                         var URL = editor.getOpt('UMEDITOR_HOME_URL'),
