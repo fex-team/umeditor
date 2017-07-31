43a1c94820482faabe201ee234ce680ed2c36e5d
Phinome <Long.phinome@gmail.com>
2017-07-31 13:54:49 +0800

    add xss filter notice

--- php/imageUp.php
+++ php/imageUp.php
@@ -16,6 +16,7 @@
     $config[ "savePath" ] = $Path;
     $up = new Uploader( "upfile" , $config );
     $type = $_REQUEST['type'];
+    // callback 部分需要自行对用户提交的内容进行过滤，防止 XSS 漏洞。
     $callback=$_GET['callback'];
 
     $info = $up->getFileInfo();
